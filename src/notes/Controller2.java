c300723c2bb2d6bec5ba238cc5f0dea9fb910213
Unknown <begue.armand@gmail.com>
2017-12-15 16:04:54 +0100

    Ajout de la 2e page
    Vue2 + Controller2 + images

--- src/notes/Controller2.java
+++ src/notes/Controller2.java
@@ -5,12 +5,16 @@
  */
 package notes;
 
+import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -22,23 +26,40 @@ import javafx.scene.layout.VBox;
  *
  * @author ArthurJCQ
  */
-public class Controller implements Initializable {
-
+public class Controller2 implements Initializable {
+    
     @FXML
-    private VBox pp_haut = new VBox(), pp_milieu = new VBox(), pp_connexion = new VBox(), pp_bas = new VBox();
+    private VBox pp_bas = new VBox();
+    
+    @FXML
+    private VBox p2_haut = new VBox(), p2_milieu = new VBox();
+    
+    @FXML
+    private ComboBox<String> listematieres, listeseances;
+        
+    @FXML
+    private Button boutonacces = new Button();
+    
+    @FXML
+    private Label matiere = new Label(), seance = new Label();
     
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        pp_haut.setPrefSize(1600, 875);
-        pp_milieu.setPrefSize(1600, 675);
+        
         pp_bas.setStyle("-fx-background-color: #cccccc;");
         pp_bas.setPrefSize(1600, 25);
-        pp_connexion.setStyle("-fx-border-color: black;-fx-border-width: 2;-fx-border-radius: 15;");
-        pp_connexion.setMaxSize(300.0, 300.0);
-        pp_connexion.setMinSize(300.0, 300.0);
+        
+        p2_haut.setPrefSize(1600, 200);
+        p2_milieu.setPrefSize(1600, 675);
+        p2_milieu.setSpacing(25);
+        p2_milieu.setPadding(new Insets(190, 0, 0, 0));
+        
+        listematieres.setPrefSize(150, 40);
+        
+        listeseances.setPrefSize(150, 40);
     }    
     
 }
